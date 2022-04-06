dd46336e176f86f129ea7fe6eb594d9d37ca1c9a
ichengi <39175857+ichengi@users.noreply.github.com>
2022-04-06 17:56:07 +0800

    Update main.py
    添加按钮的判断

--- main.py
+++ main.py
@@ -15,21 +15,28 @@ def startProgramDefault(ProgramSettings: dict) -> int:
 
         return open_state.returncode
 
-class MainWindow():
+class MainWindow(QMainWindow):
     def __init__(self):
-        self.ui = QUiLoader().load('main.ui')
+        super().__init__()
+        # self.ui = QUiLoader().load('main.ui')
+        self.ui = Ui_MainWindow()
+        # 初始化界面
+        self.ui.setupUi(self)
         self.ui.StartButton.clicked.connect(self.handleStart)
 
     def handleStart(self):
         with open("res\ProgramConfig.json", "r") as json_file:
             programdata = json.load(json_file)
-        testdic = programdata['Programs']['MAA']  # 做测试的一个程序
-        states = startProgramDefault(testdic)  # 判断程序打开
-        sleep(2)  # todo 暂时未判断窗口是否显示，拿个延时顶一哈，可能考虑加个死循环，或者找其他方法
-        for operation_item in testdic['operation']:  # 执行点击命令
-            order = f"""pyautogui.{operation_item['operationaction']}("{operation_item['operationimg']}")"""
-            exec(order)
 
+        for item in self.ui.Prolist.buttons():  # 遍历选中的按钮
+            if item.isChecked() and item.text() in programdata['Programs']:
+                prodataitem = programdata['Programs'][item.text()]
+                states = startProgramDefault(prodataitem)  # 判断程序打开
+                sleep(3)  # todo 暂时未判断窗口是否显示，拿个延时顶一哈，可能考虑加个死循环，或者找其他方法
+                for operation_item in prodataitem['operation']:  # 执行点击命令
+                    order = f"""pyautogui.{operation_item['operationaction']}("{operation_item['operationimg']}")"""
+                    exec(order)
+                    
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
