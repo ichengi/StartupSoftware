9df01e3667159becdd8c3ff1b2fbb99b0b880edb
chengi i <1078994941@qq.com>
2023-01-16 08:29:30 +0000

    fix:添加识别打开结果识图

--- script/wrapper.py
+++ script/wrapper.py
@@ -3,6 +3,7 @@
 使用的装饰器
 '''
 import time
+import pyautogui
 
 
 def wait_seconds(seconds:int):
@@ -15,3 +16,16 @@ def wait_seconds(seconds:int):
             return ret
         return inner
     return wrapper
+
+def is_open(img_path:str):
+    """测试程序是否打开成功"""
+    def wrapper(func):
+        def inner(*args, **kwargs):
+            ret = func(*args, **kwargs)
+            charge = pyautogui.locateOnScreen(img_path, confidence=0.75, grayscale=True)
+            if charge is None:
+                return False
+            return ret
+        return inner
+    return wrapper
+
